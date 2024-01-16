f29c3f47c9664be4c3911869ebaec16fc4d17a87
orendzi <orendzi.rayon@gmail.com>
2024-01-16 23:22:30 +0400

    KS1.2 numOfEmployees and logs added

---
+++ src/main/kotlin/lsn1/tsk2.kt
@@ -0,0 +1,15 @@
+package lsn1
+
+// Lesson 1, task 2
+fun main() {
+    val numOfOrders: Int = 75
+    val msgOnPurchase: String = "Thank you for shopping in our store!"
+
+    println(numOfOrders)
+    println(msgOnPurchase)
+
+    var numOfEmployees = 2000
+//    println(numOfEmployees)
+    numOfEmployees -= 1
+    println(numOfEmployees)
+}
