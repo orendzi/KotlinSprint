6fcd0e1fa40b1248118f28d41c9348c935750e6e
orendzi <orendzi.rayon@gmail.com>
2024-01-17 21:47:46 +0400

    KS-2-4 buff computing added

---
+++ src/main/kotlin/lsn2/tsk4.kt
@@ -0,0 +1,12 @@
+package lsn2
+
+// Lesson 2, task 4
+fun main() {
+    val buff = 20
+    val ironOre = 11
+    val crystalOre = 7
+    val ironOreBuff = ironOre * buff / 100
+    val crystalOreBuff = crystalOre * buff / 100
+    println(ironOreBuff)
+    println(crystalOreBuff)
+}
