ce1d5b9046bab55e2c16c31e5f17498908880529
orendzi <orendzi.rayon@gmail.com>
2024-01-17 23:00:07 +0400

    KS-2-1 optimization of code readability and logics

--- src/main/kotlin/lsn2/tsk1.kt
+++ src/main/kotlin/lsn2/tsk1.kt
@@ -1,5 +1,11 @@
 package lsn2
 // Lesson 2, task 1
 fun main() {
-    println((3 + 4 + 3 + 5) / 4.0)
+    val numOfStudents = 4
+    val st1Res = 3
+    val st2Res = 4
+    val st3Res = 3
+    val st4Res = 5
+    val averageRes = (st1Res + st2Res + st3Res + st4Res) / numOfStudents
+    println(averageRes.toFloat())
 }
