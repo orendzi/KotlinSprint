01b6cd1337875ea0271ec3126f1d8e5950dd9c03
orendzi <orendzi.rayon@gmail.com>
2024-01-17 15:23:55 +0400

    KS-2-2 accounting task

---
+++ src/main/kotlin/lsn2/tsk2.kt
@@ -0,0 +1,10 @@
+package lsn2
+
+// Lesson 2, task 2
+fun main() {
+    val employeesWaste = 50 * 30000
+    val traineesWaste = 30 * 20000
+    val totalWaste = employeesWaste + traineesWaste
+    val averageSalary = totalWaste / (50 + 30)
+    println("$employeesWaste\n$totalWaste\n$averageSalary")
+}
