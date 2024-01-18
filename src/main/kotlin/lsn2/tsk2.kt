6645ac035d4bcfac5df98abefbf2d52be4e37d23
orendzi <orendzi.rayon@gmail.com>
2024-01-18 11:46:30 +0400

    KS-2-2 vars for employees and trainees created

--- src/main/kotlin/lsn2/tsk2.kt
+++ src/main/kotlin/lsn2/tsk2.kt
@@ -2,9 +2,11 @@ package lsn2
 
 // Lesson 2, task 2
 fun main() {
-    val employeesWaste = 50 * 30000
-    val traineesWaste = 30 * 20000
+    val employees = 50
+    val trainees = 30
+    val employeesWaste = employees * 30000
+    val traineesWaste = trainees * 20000
     val totalWaste = employeesWaste + traineesWaste
-    val averageSalary = totalWaste / (50 + 30)
+    val averageSalary = totalWaste / (employees + trainees)
     println("$employeesWaste\n$totalWaste\n$averageSalary")
 }
