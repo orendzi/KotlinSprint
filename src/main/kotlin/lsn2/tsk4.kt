b59c52129a08c904b21b715525429947d935f632
orendzi <orendzi.rayon@gmail.com>
2024-01-18 11:50:33 +0400

    KS-2-4 var for percents created

--- src/main/kotlin/lsn2/tsk4.kt
+++ src/main/kotlin/lsn2/tsk4.kt
@@ -2,11 +2,12 @@ package lsn2
 
 // Lesson 2, task 4
 fun main() {
+    val percentage = 100
     val buff = 20
     val ironOre = 11
     val crystalOre = 7
-    val ironOreBuff = ironOre * buff / 100
-    val crystalOreBuff = crystalOre * buff / 100
+    val ironOreBuff = ironOre * buff / percentage
+    val crystalOreBuff = crystalOre * buff / percentage
     println(ironOreBuff)
     println(crystalOreBuff)
 }
