0ebd1a4efd12bd98acf51076a13e1fc7a02e8711
orendzi <orendzi.rayon@gmail.com>
2024-01-18 18:35:09 +0400

    KS-3-1 greetings for any time added

---
+++ src/main/kotlin/lsn3/tsk1.kt
@@ -0,0 +1,10 @@
+package lsn3
+
+// Lesson 3, task 1
+fun main() {
+    val userName = "Orendzi"
+    var greetingTxt = "Good morning"
+    println("$greetingTxt, $userName!")
+    greetingTxt = "Good evening"
+    println("$greetingTxt, $userName!")
+}
