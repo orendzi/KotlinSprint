2158c1c25e7b8e2c37ba45f18c6f37b921913aa8
orendzi <orendzi.rayon@gmail.com>
2024-01-20 16:26:00 +0400

    KS-4-2 constants added and vars renamed

--- src/main/kotlin/lsn4/tsk2.kt
+++ src/main/kotlin/lsn4/tsk2.kt
@@ -1,18 +1,18 @@
 package lsn4
 
 // Lesson 4, task 2
+const val CAT_AVERAGE_MIN_WEIGHT = 35
+const val CAT_AVERAGE_MAX_WEIGHT = 100
+const val CAT_AVERAGE_VOLUME = 100
 fun main() {
-    val catAverage_minWeight = 35
-    val catAverage_maxWeight = 100
-    val catAverage_Volume = 100
-    val cargo1_Weight = 20
-    val cargo1_Volume = 80
-    val cargo2_Weight = 50
-    val cargo2_Volume = 100
+    val cargo1Weight = 20
+    val cargo1Volume = 80
+    val cargo2Weight = 50
+    val cargo2Volume = 100
 
-    println("Груз с весом $cargo1_Weight кг и объёмом $cargo1_Volume л сооветствует категории 'Average': " +
-            "${cargo1_Weight >= catAverage_minWeight && cargo1_Weight <= catAverage_maxWeight && cargo1_Volume < catAverage_Volume}.")
+    println("Груз с весом $cargo1Weight кг и объёмом $cargo1Volume л сооветствует категории 'Average': " +
+            "${cargo1Weight >= CAT_AVERAGE_MIN_WEIGHT && cargo1Weight <= CAT_AVERAGE_MAX_WEIGHT && cargo1Volume < CAT_AVERAGE_VOLUME}.")
 
-    println("Груз с весом $cargo2_Weight кг и объёмом $cargo2_Volume л сооветствует категории 'Average': " +
-            "${cargo2_Weight >= catAverage_minWeight && cargo2_Weight <= catAverage_maxWeight && cargo2_Volume < catAverage_Volume}.")
+    println("Груз с весом $cargo2Weight кг и объёмом $cargo2Volume л сооветствует категории 'Average': " +
+            "${cargo2Weight >= CAT_AVERAGE_MIN_WEIGHT && cargo2Weight <= CAT_AVERAGE_MAX_WEIGHT && cargo2Volume < CAT_AVERAGE_VOLUME}.")
 }
